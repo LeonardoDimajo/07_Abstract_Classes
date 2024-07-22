2095253dfab5c8c166994f7e961eada62718561a
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-22 11:07:46 +0200

    03 Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,33 @@ public class App {
     
     public static void main(String[] args) {
      
-       
+       Rectangle rectangle = new Rectangle(10.0, 10.0);
+       Circle circle = new Circle(5.0);
+       Triangle triangle = new Triangle(10.0, 10.0);
+
+
+       output("------ Flächen ------"); 
+       output("Fläche Rechteck: " + rectangle.area());
+       output("Fläche Kreis: " + circle.area());
+       output("Fläche Dreieck: " + triangle.area());
+
+    
+        output("------  Differenzen ------");
+        output("Diff. r-t :" + areaDiff(rectangle, triangle));
+        output("Diff. r-c :" + areaDiff(rectangle, circle));
+
+
+
     }
 
-  
+  private static double areaDiff(Rectangle r, Triangle t){
+    return r.area() - t.area();
+
+  }
+
+    private static double areaDiff(Rectangle r, Circle c){
+        return r.area() - c.area();
+  }
 
    
 
